90311de1f9086d769fb49aeaec8a416dd4fc391f
liutaochange <liutaochange@163.com>
2018-09-06 22:54:26 +0800

    api config

---
+++ src/api/config.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'localhost:9099/api'
+export const config = {
+  code: 1
+}
--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,17 @@
 /**
  * Created by LiuTao on 2018/4/10.
  */
+import axios from 'axios'
+import { baseUrl } from './config'
+const request = axios.create({
+  baseURL: baseUrl,
+  timeout: 1000,
+  withCredentials: true
+})
+// login 
+export const login = (params) => {
+  const url = '/login'
+  return request.get(url, {
+    params: params
+  })
+}
