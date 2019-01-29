555012170ff9611f74cd9300cf6a14f306fb86d3
liutaochange <liangliutao@qiyi.com>
2019-01-29 11:27:30 +0800

    Revert "fix"
    This reverts commit 89590ae60b5103e4e10c0615326af5c12dbf2d13.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Index from '@/components/index'
+import Login from '@/components/login'
+import Home from '@/components/home'
+import Sider from '@/components/slider'
 Vue.use(Router)
 
 export default new Router({
@@ -11,17 +15,22 @@ export default new Router({
     {
       path: '/index',
       name: 'index',
-      component: () => import('@/components/index')
+      component: Index
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/components/login')
+      component: Login
     },
     {
       path: '/home',
       name: 'home',
-      component: () => import('@/components/home')
+      component: Home
+    },
+    {
+      path: '/slider',
+      name: 'slider',
+      component: Slider
     }
   ]
 })
