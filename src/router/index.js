69daae424fe8783135701c4edb7d5eede9adebb0
liutaochange <liutaochange@163.com>
2019-07-10 17:35:15 +0800

    feat: 路由控制

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import 'muse-ui-message/dist/muse-ui-message.css'
-import Message from 'muse-ui-message'
-Vue.use(Router)
-Vue.use(Message)
+import Vue from 'vue';
+import Router from 'vue-router';
+import 'muse-ui-message/dist/muse-ui-message.css';
+import Message from 'muse-ui-message';
+import Store from '@/utils/store';
+Vue.use(Router);
+Vue.use(Message);
 
 const router = new Router({
   base: process.env.NODE_ENV === 'production' ? '/chatting/' : '/',
@@ -11,7 +12,7 @@ const router = new Router({
   routes: [
     {
       path: '/',
-      redirect: '/index.html'
+      redirect: '/index.html',
     },
     {
       path: '/index.html',
@@ -22,36 +23,40 @@ const router = new Router({
         {
           path: '/home.html',
           name: 'home',
-          component: () => import('@/pages/home')
+          component: () => import('@/pages/home'),
         },
         {
           path: '/mine.html',
           name: 'mine',
-          component: () => import('@/pages/mine')
-        }
-      ]
+          component: () => import('@/pages/mine'),
+        },
+      ],
     },
     {
       path: '/login.html',
       name: 'login',
-      component: () => import('@/pages/login')
+      component: () => import('@/pages/login'),
     },
     {
       path: '/register.html',
       name: 'register',
-      component: () => import('@/pages/register')
-    }
-  ]
-})
+      component: () => import('@/pages/register'),
+    },
+  ],
+});
 router.beforeEach((to, from, next) => {
   if (to.name === 'mine') {
-    Message.alert('请先登录', '提示').then(res => {
-      if (res.result) {
-        router.push('/login.html')
-      }
-    })
+    if (Store.get('__USER_INFO__') && Store.get('__USER_INFO__').token !== '') {
+      next();
+    } else {
+      Message.alert('请先登录', '提示').then(res => {
+        if (res.result) {
+          router.push('/login.html');
+        }
+      });
+    }
   } else {
-    next()
+    next();
   }
-})
-export default router
+});
+export default router;
